12fe6d0c140f3002918f94c6498d65aeb6df75bc
Russ <sayersr@amazon.com>
2021-05-18 14:27:29 +1000

    initial commit

---
+++ tests.py
@@ -0,0 +1,23 @@
+def test1():
+    print("hello I'm test1")
+    pass
+
+def test2():
+    print("hello I'm test2")
+    pass
+
+def test3():
+    print("hello I'm test3")
+    pass
+
+def test4():
+    print("hello I'm test4")
+    pass
+
+def test5():
+    print("hello I'm test5")
+    pass
+
+def test6():
+    print("hello I'm test6")
+    pass
